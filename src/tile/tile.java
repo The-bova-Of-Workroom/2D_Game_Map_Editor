354605b0d76df71a59335628237e7a4c5809a8eb
bova <60965111+The-bova-Of-Workroom@users.noreply.github.com>
2020-02-16 12:18:07 +0900

    Update tile.java

--- src/tile/tile.java
+++ src/tile/tile.java
@@ -9,7 +9,7 @@ public class tile {
 
     public tile() {
         tsRoad = new tStructure(32, 32, 16, 6, "/ROAD_32x32-16x6.png");
-        tsTile = new tStructure(8, 8, 16, 6, "/TILE_8x8-16x6.png");
+        tsTile = new tStructure(16, 16, 16, 6, "/TILE_16x16-16x6.png");
         ts = tsTile;
     }
 }
